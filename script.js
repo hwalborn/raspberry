7e065367dad373ea8a573a04c5fb01d71b4f7c85
hwalborn <hwalborn@gmail.com>
2017-03-29 13:15:21 -0400

    added colors and details to wifihotspots

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-var hotspotArray
+var hotspotArray, lat, long, latLong, marker
+
 var showHotspots = () => {
-  $.get('https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD', (response) =>{
-    return response["data"]
-  }).done((locations) => {
-    hotspotArray =  locations["data"]
-  })
+  $.get('https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD')
+  .then((locations) => {
+    hotspotArray = locations["data"]
+  }).then(navigator.geolocation.getCurrentPosition(success))
 }
 
-showHotspots()
-
 class wifiHotspot {
-   marker(lat, long, map){
-    return new google.maps.Marker({
+  constructor(location, deets){
+    this.location = location
+    this.deets = deets
+  }
+   marker(lat, long, map, icon){
+    marker = new google.maps.Marker({
       position: {lat: lat, lng: long},
-      map: map
+      map: map,
+      icon: icon
     })
+    google.maps.event.addListener(marker, 'click', () => {
+      alert(this.location)
+    })
+    return marker
   }
 }
 
-var success = (position) => {
-  var lat = position.coords.latitude
-  var long = position.coords.longitude
-  var latLong = {lat: lat, lng: long}
 
+
+/////Google Map Stuff//////
+var initGoogleMap = () => {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: new google.maps.LatLng(lat,long),
@@ -30,17 +36,27 @@ var success = (position) => {
   })
 
   new google.maps.Marker({
-      position: latLong,
-      map: map
+    position: latLong,
+    map: map
   });
+
+  var pinColor = "0c0";
+  var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor)
 
   hotspotArray.forEach((hotspot) => {
     if((parseFloat(hotspot[15]) < lat + 0.01 && parseFloat(hotspot[15]) > lat - 0.01) && (parseFloat(hotspot[16]) < long + 0.01 && parseFloat(hotspot[16]) > long - 0.01)){
-      var x = new wifiHotspot
-      x.marker(parseFloat(hotspot[15]), parseFloat(hotspot[16]), map)
+      var x = new wifiHotspot(hotspot[13], hotspot[8])
+      x.marker(parseFloat(hotspot[15]), parseFloat(hotspot[16]), map, pinImage)
     }
   })
-
 }
 
+//////Geolocation Stuff/////
+var success = (position) => {
+  lat = position.coords.latitude
+  long = position.coords.longitude
+  latLong = {lat: lat, lng: long}
+  initGoogleMap()
+}
 navigator.geolocation.getCurrentPosition(success)
+showHotspots()
