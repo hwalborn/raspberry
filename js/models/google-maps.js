e11a0968efa404e2558c196c14e19d563c775a42
hwalborn <hwalborn@gmail.com>
2017-03-31 11:42:53 -0400

    made loading pages and notation on code

--- js/models/google-maps.js
+++ js/models/google-maps.js
@@ -1,4 +1,7 @@
-var markers = [];
+/////Make it so that all we need is a longitude and latitude
+/////to create a new google map and display it on the page
+/////with all of the hotspot markers
+
 class GoogleMap{
   constructor(lat, long, hotspotArray, maptype){
     debugger
@@ -11,8 +14,9 @@ class GoogleMap{
 
   }
 
-
   initGoogleMap() {
+    ////Make new instance of the google map class from
+    ////Google Maps API
      var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: new google.maps.LatLng(this.lat,this.long),
@@ -23,19 +27,22 @@ class GoogleMap{
          map.setTilt(45);
     }
 
+    ////Make marker for your current position
     myMarker = new google.maps.Marker({
       position: this.latLong,
       map: map,
       draggable: true
     });
 
+    ////Change the google map if the current position marker
+    ////is dragged
     google.maps.event.addListener(myMarker,'dragend',() => {
       lat = myMarker.position.lat();
       long = myMarker.position.lng();
-      debugger
       new GoogleMap(lat,long,this.hotspotArray,this.maptype)
     })
 
+    ////display the hotspot markers on the current map
     wifiHotspot.display(this.hotspotArray, map)
   }
 }
--- js/wrappers/api.js
+++ js/wrappers/api.js
@@ -1,3 +1,5 @@
+/////Call and parse the NYC Open Data free wifi API
+
 class WifiApi{
   static getJSON(){
     return $.getJSON('https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD')
